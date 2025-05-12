1e72fa3ba081816d6704853c2984ef8b3150593f
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-12 11:34:24 -0300

    feat: class component

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import  Multi, { BoaNoite } from './componentes/Multiplos'
+import  Saudacao from './componentes/Saudacao'
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome="Fellype"/>
-        < BoaNoite nome="Giu"/>
+        <Saudacao tipo="Bom dia" nome="Fellype" />
     </div>
     , document.getElementById('root'))
