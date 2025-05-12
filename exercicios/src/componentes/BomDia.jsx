017fe60a74fe619a9303aaec0a24448678bbd132
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-05-12 10:03:30 -0300

    feat: component with parameter

---
+++ exercicios/src/componentes/BomDia.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default props => [
+    <h1 key="h1">Bom dia {props.nome}!</h1>,
+    <h2 key="h2">até Logo</h2>
+]
+
+
+
+// export default props =>
+// <React.Fragment> ou <div>
+//     <h1>Bom dia {props.nome}!</h1>
+//     <h2>até Logo</h2>
+// </React.Fragment>
+
